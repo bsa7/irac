2345ff8cc691c3299b5356135f74f64603b659fd
Belevskij Sergeij <bbelevskij@gmail.com>
2020-06-15 12:30:56 +0500

    Update Readme, fix coverage reporting

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,5 +1,7 @@
-const cypressTypeScriptPreprocessor = require('./cy-ts-preprocessor')
+module.exports = (on, config) => {
+  require('@cypress/code-coverage/task')(on, config)
+  on('file:preprocessor', require('@cypress/code-coverage/use-babelrc'))
+  // on('file:preprocessor', require('./cy-ts-preprocessor'))
 
-module.exports = on => {
-  on('file:preprocessor', cypressTypeScriptPreprocessor)
+  return config
 }
